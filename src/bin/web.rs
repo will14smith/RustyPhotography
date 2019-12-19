e1194074d80db6a6ca70466c09c7f4710c22e64e
Will Smith <will@toxon.co.uk>
2019-12-19 14:37:22 +0000

    Update the lambda implementation to work

--- src/bin/web.rs
+++ src/bin/web.rs
@@ -1,8 +1,14 @@
 use rocket_lamb::RocketExt;
 use photography::create_rocket;
+use photography::data::Client;
+use rusoto_core::Region;
+use rusoto_dynamodb::DynamoDbClient;
 
 fn main() {
-    create_rocket()
+    let dynamo = DynamoDbClient::new(Region::default());
+    let client = Client::new(dynamo);
+
+    create_rocket(client)
         .lambda()
         .launch();
 }
